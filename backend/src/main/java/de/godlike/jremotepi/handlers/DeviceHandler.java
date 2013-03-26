16fca7e2670f98b70ab0abcaf567dab28e0bbfce
Tobias Schneider <asmodaeus.evo1@gmail.com>
2013-03-26 22:09:36 +0100

    refactoring, cleanup and html demo

--- backend/src/main/java/de/godlike/jremotepi/handlers/DeviceHandler.java
+++ backend/src/main/java/de/godlike/jremotepi/handlers/DeviceHandler.java
@@ -8,12 +8,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriBuilderException;
+import javax.ws.rs.core.UriInfo;
 
 import de.godlike.jremotepi.business.dtos.Body;
 import de.godlike.jremotepi.business.dtos.Button;
@@ -27,39 +33,33 @@ import de.godlike.jremotepi.persistence.entities.Device;
 
 @Path("/device")
 public class DeviceHandler {
+	@Context
+	UriInfo uriInfo;
+
 	@SuppressWarnings("unchecked")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public DeviceOverview listAllDevices() {
+	private DeviceOverview getDevicesOverview() {
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory("jRemotePi");
 		EntityManager em = factory.createEntityManager();
 		try {
 			Query q = em.createQuery("select d from Device d");
 			List<Device> allDevices = q.getResultList();
-			// for (Device device : allDevices) {
-			// try {
-			// UriBuilder path =
-			// UriBuilder.fromResource(this.getClass()).path(this.getClass().getMethod("switchDevice",
-			// String.class, String.class)).build("", "");
-			// } catch (IllegalArgumentException e) {
-			// // TODO Auto-generated catch block
-			// e.printStackTrace();
-			// } catch (NoSuchMethodException e) {
-			// // TODO Auto-generated catch block
-			// e.printStackTrace();
-			// } catch (SecurityException e) {
-			// // TODO Auto-generated catch block
-			// e.printStackTrace();
-			// }
-			// }
+
 			List<Button> navbarButtons = new ArrayList<Button>();
 			navbarButtons.add(new Button("Geräte", "uri..", "NEWPAGE"));
 			navbarButtons.add(new Button("System Infos", "uri..", "NEWPAGE"));
 
-			List<Element> contentContent = new ArrayList<Element>();
-			contentContent.add(new Text("Test TesT TEST"));
-			contentContent.add(new Button("ContentButton", "uri..", "ACTION"));
+			List<Element> bodyContent = new ArrayList<Element>();
+			for (Device device : allDevices) {
+				bodyContent.add(new Text(device.getName()));
+				bodyContent.add(new Button("on", UriBuilder
+						.fromResource(this.getClass())
+						.path(this.getClass().getMethod("switchDevice",
+								String.class, String.class))
+						.build(device.getId(), "1").toASCIIString(), "ACTION"));
+				bodyContent.add(new Button("off", "http://localhost:8080/"
+						+ device.getId() + "/0/", "ACTION"));
+			}
 
 			List<Element> footerContent = new ArrayList<Element>();
 			footerContent.add(new Text("Test TesT TEST"));
@@ -67,22 +67,31 @@ public class DeviceHandler {
 
 			List<OverElement> overElement = new ArrayList<OverElement>();
 			overElement.add(new NavBar(navbarButtons));
-			overElement.add(new Body(contentContent));
+			overElement.add(new Body(bodyContent));
 			overElement.add(new Footer(footerContent));
 
 			DeviceOverview overview = new DeviceOverview(overElement);
 
 			return overview;
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UriBuilderException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			em.close();
 		}
+		return new DeviceOverview();
 	}
 
-	@GET
-	@Path("/{systemId}/{deviceId}/{state}")
-	public void switchDevice(@PathParam("systemId") String systemId,
-			@PathParam("deviceId") String deviceId,
-			@PathParam("state") String state) {
+	private void switchDevice(String systemId, String deviceId, String state) {
 		ProcessBuilder sendProcessBuilder = new ProcessBuilder(
 				"/usr/sbin/send", systemId, deviceId, state);
 		try {
@@ -90,6 +99,35 @@ public class DeviceHandler {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Device> listAllDevices() {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("jRemotePi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			Query q = em.createQuery("select d from Device d");
+			return q.getResultList();
+		} finally {
+			em.close();
+		}
+	}
+
+	@GET
+	@Path("/{entityId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Device getDevice(@PathParam("entityId") String entityId) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("jRemotePi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			return em.find(Device.class, Long.parseLong(entityId));
+		} finally {
+			em.close();
 		}
 	}
 
@@ -109,12 +147,35 @@ public class DeviceHandler {
 		}
 	}
 
+	// @PUT
+	// @Path("/{deviceName}/{systemId}/{deviceId}")
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public Device putDevice(@PathParam("deviceName") String deviceName,
+	// @PathParam("systemId") String systemId,
+	// @PathParam("deviceId") String deviceId) {
+	// EntityManagerFactory factory = Persistence
+	// .createEntityManagerFactory("jRemotePi");
+	// EntityManager em = factory.createEntityManager();
+	// try {
+	// em.getTransaction().begin();
+	// Device newDevice = new Device();
+	// newDevice.setName(deviceName);
+	// newDevice.setSystemId(systemId);
+	// newDevice.setDeviceId(deviceId);
+	// Device mergedDevice = em.merge(newDevice);
+	// em.getTransaction().commit();
+	//
+	// return em.find(Device.class, mergedDevice.getId());
+	// } finally {
+	// em.close();
+	// }
+	// }
+
 	@PUT
-	@Path("/{deviceName}/{systemId}/{deviceId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Device putDevice(@PathParam("deviceName") String deviceName,
-			@PathParam("systemId") String systemId,
-			@PathParam("deviceId") String deviceId) {
+	public Device putDevice(@FormParam("deviceName") String deviceName,
+			@FormParam("systemId") String systemId,
+			@FormParam("deviceId") String deviceId) {
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory("jRemotePi");
 		EntityManager em = factory.createEntityManager();
@@ -133,21 +194,21 @@ public class DeviceHandler {
 		}
 	}
 
-	// @DELETE
-	// @Path("/{deviceId}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public List<Device> deleteDevice(@PathParam("deviceId") Long deviceId) {
-	// EntityManagerFactory factory = Persistence
-	// .createEntityManagerFactory("jRemotePi");
-	// EntityManager em = factory.createEntityManager();
-	// try {
-	// em.getTransaction().begin();
-	// em.remove(em.find(Device.class, deviceId));
-	// em.getTransaction().commit();
-	//
-	// return this.listAllDevices();
-	// } finally {
-	// em.close();
-	// }
-	// }
+	@DELETE
+	@Path("/{deviceId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Device> deleteDevice(@PathParam("deviceId") Long deviceId) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("jRemotePi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.remove(em.find(Device.class, deviceId));
+			em.getTransaction().commit();
+
+			return this.listAllDevices();
+		} finally {
+			em.close();
+		}
+	}
 }
---
+++ backend/src/main/webapp/main.js
@@ -0,0 +1,13 @@
+function  DeviceList ($scope, $http) {
+	
+	this.url = "http://localhost:8080/backend/resources/device/";
+
+		$http.get(this.url).
+	  success(function(data, status, headers, config) {
+	    $scope.list = data.device;
+	  }).
+	  error(function(data, status, headers, config) {
+		  $scope.result = "fehler";
+	  });
+	
+}
