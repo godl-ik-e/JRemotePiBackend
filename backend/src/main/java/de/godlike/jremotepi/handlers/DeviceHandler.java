8697f438a27a2027ad30d108d2f5b73bd8db9282
Tobias Schneider <asmodaeus.evo1@gmail.com>
2013-04-09 19:55:27 +0200

    refactoring DeviceHandler

--- backend/src/main/java/de/godlike/jremotepi/handlers/DeviceHandler.java
+++ backend/src/main/java/de/godlike/jremotepi/handlers/DeviceHandler.java
@@ -1,7 +1,6 @@
 package de.godlike.jremotepi.handlers;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -11,85 +10,17 @@ import javax.persistence.Query;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
-import javax.ws.rs.core.UriBuilderException;
-import javax.ws.rs.core.UriInfo;
 
-import de.godlike.jremotepi.business.dtos.Body;
-import de.godlike.jremotepi.business.dtos.Button;
-import de.godlike.jremotepi.business.dtos.DeviceOverview;
-import de.godlike.jremotepi.business.dtos.Element;
-import de.godlike.jremotepi.business.dtos.Footer;
-import de.godlike.jremotepi.business.dtos.NavBar;
-import de.godlike.jremotepi.business.dtos.OverElement;
-import de.godlike.jremotepi.business.dtos.Text;
 import de.godlike.jremotepi.persistence.entities.Device;
 
 @Path("/device")
 public class DeviceHandler {
-	@Context
-	UriInfo uriInfo;
-
-	@SuppressWarnings("unchecked")
-	private DeviceOverview getDevicesOverview() {
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("jRemotePi");
-		EntityManager em = factory.createEntityManager();
-		try {
-			Query q = em.createQuery("select d from Device d");
-			List<Device> allDevices = q.getResultList();
-
-			List<Button> navbarButtons = new ArrayList<Button>();
-			navbarButtons.add(new Button("Geräte", "uri..", "NEWPAGE"));
-			navbarButtons.add(new Button("System Infos", "uri..", "NEWPAGE"));
-
-			List<Element> bodyContent = new ArrayList<Element>();
-			for (Device device : allDevices) {
-				bodyContent.add(new Text(device.getName()));
-				bodyContent.add(new Button("on", UriBuilder
-						.fromResource(this.getClass())
-						.path(this.getClass().getMethod("switchDevice",
-								String.class, String.class))
-						.build(device.getId(), "1").toASCIIString(), "ACTION"));
-				bodyContent.add(new Button("off", "http://localhost:8080/"
-						+ device.getId() + "/0/", "ACTION"));
-			}
-
-			List<Element> footerContent = new ArrayList<Element>();
-			footerContent.add(new Text("Test TesT TEST"));
-			footerContent.add(new Text("Test TesT TEST"));
-
-			List<OverElement> overElement = new ArrayList<OverElement>();
-			overElement.add(new NavBar(navbarButtons));
-			overElement.add(new Body(bodyContent));
-			overElement.add(new Footer(footerContent));
-
-			DeviceOverview overview = new DeviceOverview(overElement);
-
-			return overview;
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UriBuilderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			em.close();
-		}
-		return new DeviceOverview();
-	}
 
 	private void switchDevice(String systemId, String deviceId, String state) {
 		ProcessBuilder sendProcessBuilder = new ProcessBuilder(
@@ -118,58 +49,34 @@ public class DeviceHandler {
 	}
 
 	@GET
-	@Path("/{entityId}")
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Device getDevice(@PathParam("entityId") String entityId) {
+	public Device getDevice(@PathParam("id") String id) {
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory("jRemotePi");
 		EntityManager em = factory.createEntityManager();
 		try {
-			return em.find(Device.class, Long.parseLong(entityId));
+			return em.find(Device.class, Long.parseLong(id));
 		} finally {
 			em.close();
 		}
 	}
 
 	@GET
-	@Path("/{entityId}/{state}")
-	public void switchDevice(@PathParam("entityId") String entityId,
+	@Path("/{id}/{state}")
+	public void switchDevice(@PathParam("id") Long id,
 			@PathParam("state") String state) {
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory("jRemotePi");
 		EntityManager em = factory.createEntityManager();
 		try {
-			Device deviceToSwitch = em.find(Device.class, entityId);
+			Device deviceToSwitch = em.find(Device.class, id);
 			this.switchDevice(deviceToSwitch.getSystemId(),
 					deviceToSwitch.getDeviceId(), state);
 		} finally {
 			em.close();
 		}
 	}
-
-	// @PUT
-	// @Path("/{deviceName}/{systemId}/{deviceId}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// public Device putDevice(@PathParam("deviceName") String deviceName,
-	// @PathParam("systemId") String systemId,
-	// @PathParam("deviceId") String deviceId) {
-	// EntityManagerFactory factory = Persistence
-	// .createEntityManagerFactory("jRemotePi");
-	// EntityManager em = factory.createEntityManager();
-	// try {
-	// em.getTransaction().begin();
-	// Device newDevice = new Device();
-	// newDevice.setName(deviceName);
-	// newDevice.setSystemId(systemId);
-	// newDevice.setDeviceId(deviceId);
-	// Device mergedDevice = em.merge(newDevice);
-	// em.getTransaction().commit();
-	//
-	// return em.find(Device.class, mergedDevice.getId());
-	// } finally {
-	// em.close();
-	// }
-	// }
 
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
@@ -194,16 +101,41 @@ public class DeviceHandler {
 		}
 	}
 
-	@DELETE
-	@Path("/{deviceId}")
+	@POST
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Device> deleteDevice(@PathParam("deviceId") Long deviceId) {
+	public Device updateDevice(@PathParam("id") Long id,
+			@FormParam("deviceName") String deviceName,
+			@FormParam("systemId") String systemId,
+			@FormParam("deviceId") String deviceId) {
 		EntityManagerFactory factory = Persistence
 				.createEntityManagerFactory("jRemotePi");
 		EntityManager em = factory.createEntityManager();
 		try {
 			em.getTransaction().begin();
-			em.remove(em.find(Device.class, deviceId));
+			Device device2Update = em.find(Device.class, id);
+			device2Update.setName(deviceName);
+			device2Update.setSystemId(systemId);
+			device2Update.setDeviceId(deviceId);
+			Device mergedDevice = em.merge(device2Update);
+			em.getTransaction().commit();
+
+			return em.find(Device.class, mergedDevice.getId());
+		} finally {
+			em.close();
+		}
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Device> deleteDevice(@PathParam("id") Long id) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("jRemotePi");
+		EntityManager em = factory.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.remove(em.find(Device.class, id));
 			em.getTransaction().commit();
 
 			return this.listAllDevices();
