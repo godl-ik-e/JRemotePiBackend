77bc9d0bf2d09f7381717ae5b9693f53148b5d0f
markus <markus@localhost>
2013-04-09 20:37:27 +0200

    Buttons

--- backend/src/main/webapp/page/js/app.js
+++ backend/src/main/webapp/page/js/app.js
@@ -3,5 +3,6 @@ angular.module('GoRemotePi', []).
   $routeProvider.
       when('/device', {templateUrl: 'partials/device-list.html',   controller: DeviceListCtrl}).
       when('/device/:deviceId', {templateUrl: 'partials/device-detail.html', controller: DeviceDetailCtrl}).
+      when('/newDevice', {templateUrl: 'partials/device-new.html', controller: DeviceNewCtrl}).
       otherwise({redirectTo: '/device'});
 }]);
--- backend/src/main/webapp/page/js/controller.js
+++ backend/src/main/webapp/page/js/controller.js
@@ -1,6 +1,38 @@
-function  DeviceListCtrl ($scope, $http) {
+function  DeviceListCtrl ($scope, $http, $location) {
+	$scope.url = "http://localhost:8080/backend/resources/device";
 	
-	this.url = "http://localhost:8080/backend/resources/device/";
+	$scope.newDevice = function(){
+		$location.path("/newDevice");
+	};
+	
+	$scope.editDevice = function(device){
+		$location.path("/device/"+device.id);
+	};
+	
+	
+	$scope.switchOn = function(device){
+		$http.get($scope.url+"/"+device.id+"/1").
+		  success(function(data, status, headers, config) {
+		    $scope.result = data;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.result = "fehler";
+		  });
+		
+	};
+	
+	$scope.switchOff = function(device){
+		$http.get($scope.url+"/"+device.id+"/0").
+		  success(function(data, status, headers, config) {
+		    $scope.result = data;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.result = "fehler";
+		  });
+		
+	};
+	
+	this.url = "http://localhost:8080/backend/resources/device";
 
 		$http.get(this.url).
 	  success(function(data, status, headers, config) {
@@ -12,8 +44,52 @@ function  DeviceListCtrl ($scope, $http) {
 	
 }
 
-function  DeviceDetailCtrl ($scope, $http) {
+function  DeviceDetailCtrl ($scope, $routeParams, $http, $location) {
 	
-	return
+	$scope.device = {};
+	  $scope.deviceId = $routeParams.deviceId;
+		$scope.url = "http://localhost:8080/backend/resources/device";
+		
+		$http.get($scope.url+"/"+$scope.deviceId).
+		  success(function(data, status, headers, config) {
+		    $scope.device = data;
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.result = "fehler";
+		  });
+		
+		$scope.save = function(device) {
+			var postdata = "deviceName="+device.name+"&systemId="+device.systemId+"&deviceId="+device.deviceId;
+			$http.post($scope.url+"/"+$scope.deviceId,postdata).
+			  success(function(data, status, headers, config) {
+				  $location.path("/device");
+			  }).
+			  error(function(data, status, headers, config) {
+				  $scope.result = "fehler";
+			  });
+		};
+		
+		$scope.delete = function(device) {
+			$http.delete($scope.url+"/"+$scope.deviceId).
+			  success(function(data, status, headers, config) {
+				  $location.path("/device");
+			  }).
+			  error(function(data, status, headers, config) {
+				  $scope.result = "fehler";
+			  });
+		};
+}
+
+function  DeviceNewCtrl ($scope, $http, $location) {
+	$scope.url = "http://localhost:8080/backend/resources/device";
+	$scope.save = function(device) {
+		$http.put($scope.url,device).
+		  success(function(data, status, headers, config) {
+			  $location.path("/device");
+		  }).
+		  error(function(data, status, headers, config) {
+			  $scope.result = "fehler";
+		  });
+	};
 	
 }
